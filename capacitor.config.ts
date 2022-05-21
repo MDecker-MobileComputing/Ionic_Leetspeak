aae878ef940a1411b459d638746e9311237740a5
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-05-21 12:24:58 +0200

    Projekt neu erstellt mit Ionic 6 -- noch keine Electron-Unterstuetzung!

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.mide.ionic.leetspeak',
+  appName: 'Leetspeak',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,111 @@
+import { Component, NgZone } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  /** String-Variable ist an TextArea zur Eingabe des zu übersetzenden Texts gebunden. */
+  public eingabeText = "";
+
+  /** String-Variable ist an TextArea zur Darstellung Übersetzungsergebnis gebunden. */
+  public ausgabeText = "";
+
+
+  /**
+   * Konstruktor für Dependency Injection.
+   *
+   * API-Doku für Klasse `ElectronService`: https://www.npmjs.com/package/ngx-electron
+   */
+  constructor(private alertCtrl: AlertController,
+              private toastController: ToastController ) {
+  }
+
+  /**
+   * Event-Handler-Methode für Button "Text in Leetspeak umwandeln".
+   */
+  private onUebersetzen() {
+
+    const inputText = this.eingabeText.trim();
+    if (inputText.length === 0) {
+
+      this.zeigeDialog("Ungültige Eingabe", "Bitte zu übersetztenden Text eingeben.");
+    }
+
+    this.ausgabeText = this.buchstabenErsetzen(inputText);
+  }
+
+  /**
+   * Methode nicht Buchstabenersetzungen für "Übersetzung" nach Leetspeak vor.
+   * Die Übersetzungsregeln entsprechen "Level 1" von https://md5decrypt.net/en/Leet-translator/
+   * (Ersetzung von "r" auf "2" wurde aber nicht übernommen).
+   *
+   * Für die Ersetzungen werden reguläre Ausdrücke der Form `/a/gi` verwendet, wobei `a` der zu
+   * ersetzende Buchstabe ist. Damit auch mehrere Vorkommen in einem String übersetzt werden,
+   * wird mit `g` die Option "global" gesetzt, und mit "i" wird die Unterscheidung von
+   * Groß-/Kleinschreibung ausgeschaltet.
+   *
+   * @param eingabe Zu übersetzender Text.
+   * @returns Als Übersetzungsergebnis anzuzeigender Text.
+   */
+  private buchstabenErsetzen(eingabe: string): string {
+
+    return eingabe.replace(/a/gi, "4")
+                  .replace(/b/gi, "8")
+                  .replace(/e/gi, "3")
+                  .replace(/g/gi, "9")
+                  .replace(/i/gi, "1")
+                  .replace(/l/gi, "1") // auch "i" wird schon auf Ziffer "1" abgebildet
+                  .replace(/o/gi, "0")
+                  .replace(/s/gi, "5")
+                  .replace(/z/gi, "2");
+  }
+
+  /**
+   * Event-Handler-Methode für Button "Text löschen". Wird auch ausgeführt, wenn Menüpunkt
+   * "Formular löschen" in Electron-Container ausgeführt wird.
+   */
+   private onLoeschen() {
+
+    this.eingabeText = "";
+    this.ausgabeText = "";
+  }
+
+ /**
+   * Hilfsmethode: Alert anzeigen, siehe auch https://ionicframework.com/docs/api/alert
+   *
+   * @param titel Titel des Dialogs
+   * @param nachricht Im Dialog anzuzeigender Text
+   */
+  async zeigeDialog(titel: string, nachricht: string) {
+
+    const meinAlert =
+          await this.alertCtrl.create({
+              header  : titel,
+              message : nachricht,
+              buttons : [ "Ok" ]
+          });
+
+    await meinAlert.present();
+  }
+
+  /**
+   * Hilfsmethode: Toast anzeigen, siehe auch https://ionicframework.com/docs/api/toast
+   *
+   * @param nachricht Im Toast anzuzeigender Text
+   */
+   async zeigeToast(nachricht: string) {
+
+    const toast =
+          await this.toastController.create({
+              message : nachricht,
+              duration: 2000  // 2000 ms = 2 seconds
+          });
+
+    await toast.present();
+  }
+
+}
